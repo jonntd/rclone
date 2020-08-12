7ec604b94c4c2713d03fcae9fe8e1c29754d50ab
Your Name <you@example.com>
2020-08-13 01:51:58 +0800

    pp

--- rclone.go
+++ rclone.go
@@ -4,10 +4,10 @@
 package main
 
 import (
-	_ "github.com/jonntd/rclone/backend/all" // import all backends
-	"github.com/jonntd/rclone/cmd"
-	_ "github.com/jonntd/rclone/cmd/all"    // import all commands
-	_ "github.com/jonntd/rclone/lib/plugin" // import plugins
+	_ "github.com/rclone/rclone/backend/all" // import all backends
+	"github.com/rclone/rclone/cmd"
+	_ "github.com/rclone/rclone/cmd/all"    // import all commands
+	_ "github.com/rclone/rclone/lib/plugin" // import plugins
 )
 
 func main() {
